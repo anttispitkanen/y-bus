975a0d06f0d69ec5c96db12f98ea03feb261a735
Antti <anttis.pitkanen@gmail.com>
2017-09-07 16:12:59 +0300

    Add menu to header.
    * Hamburger menu with animation on smaller screens.
    * Add classnames package for conditional styles.

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -5,12 +5,14 @@ import {
 } from 'react-router-dom';
 
 import MainSchedules from './mainSchedules/MainSchedules';
-import NotFound from './components/NotFound.jsx';
-import Keskusta from './views/Keskusta.jsx';
+import NotFound from './components/NotFound';
+import About from './components/About';
+import Keskusta from './views/Keskusta';
 
 // TODO: replace "/keskusta" with a generic route and component
 const Routes = (props) => (
     <Switch>
+        <Route path="/about" component={About} />
         <Route path="/:id" component={Keskusta} />
         <Route path="/" component={MainSchedules} />
         <Route path="*" component={NotFound} />
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,57 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import classNames from 'classnames';
 
 export default class Header extends React.Component {
+    constructor() {
+        super();
+        this.toggleMenu = this.toggleMenu.bind(this);
+        this.closeMenu = this.closeMenu.bind(this);
+        this.state = {
+            menuOpen: false
+        }
+    }
+
+    toggleMenu(e) {
+        this.setState({ menuOpen: !this.state.menuOpen });
+    }
+
+    closeMenu(e) {
+        this.state.menuOpen &&
+        this.setState({ menuOpen: false });
+    }
+
     render() {
         return(
-            <header>
+            <header className="header">
                 <Link to="/">
-                    <h1><span>Y</span>-bus</h1>
+                    <h1 className="main-heading"><span>Y</span>-bus</h1>
                 </Link>
+
+                <div
+                    onClick={this.toggleMenu}
+                    className="menu-button-container"
+                >
+                    <i className={classNames(
+                        'hamburger',
+                        { 'open': this.state.menuOpen },
+                        'fa',
+                        'fa-bars'
+                    )} />
+                </div>
+
+                <nav
+                    className={classNames('main-nav', { 'open': this.state.menuOpen })}
+                    onClick={this.closeMenu}
+                >
+                    <NavLink className="main-nav-link" activeClassName="active" to="/" exact>Home</NavLink>
+                    <NavLink className="main-nav-link" activeClassName="active" to="/keskusta">Keskusta</NavLink>
+                    <NavLink className="main-nav-link" activeClassName="active" to="/kauppi">Kauppi</NavLink>
+                    <NavLink className="main-nav-link" activeClassName="active" to="/hervanta">Hervanta</NavLink>
+                    <NavLink className="main-nav-link" activeClassName="active" to="/about">About</NavLink>
+                    <a className="main-nav-link" href="http://y-kampus.fi" target="_blank">Y-kampus</a>
+                </nav>
             </header>
-        )
+        );
     }
 }
