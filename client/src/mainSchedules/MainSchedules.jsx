04931b65bfbfb8be2112cc60d92d7a071b8f079d
Antti <anttis.pitkanen@gmail.com>
2017-09-07 16:34:19 +0300

    Fixes to styles in MainSchedules.

--- client/src/mainSchedules/MainSchedules.jsx
+++ client/src/mainSchedules/MainSchedules.jsx
@@ -3,18 +3,20 @@ import { unis } from '../unis.json';
 import SingleUniSchedule from './singleUniSchedule/SingleUniSchedule';
 
 const MainSchedules = () => (
-    <div className="bus-schedules">
-        <h4 className="tagline">Buses between Tampere3 universities</h4>
+    <div className="main-schedules-container">
+        <div className="main-schedules">
+            <h4 className="tagline">Buses between Tampere3 universities</h4>
 
-        { unis.map(uni => (
-            <SingleUniSchedule
-                name={uni.name}
-                key={uni.id}
-                id={uni.id}
-                imgSrc={uni.imgSrc}
-                destinations={uni.destinations}
-            />
-        ))}
+            { unis.map(uni => (
+                <SingleUniSchedule
+                    name={uni.name}
+                    key={uni.id}
+                    id={uni.id}
+                    imgSrc={uni.imgSrc}
+                    destinations={uni.destinations}
+                />
+            ))}
+        </div>
     </div>
 );
 
