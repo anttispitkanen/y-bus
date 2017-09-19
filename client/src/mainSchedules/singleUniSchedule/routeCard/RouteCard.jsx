03be19ac749a9ffc1d9852ed9a40d2def5b01418
Antti <anttis.pitkanen@gmail.com>
2017-09-19 11:40:23 +0300

    Simple logic to avoid unnecessary server calls.

--- client/src/mainSchedules/singleUniSchedule/routeCard/RouteCard.jsx
+++ client/src/mainSchedules/singleUniSchedule/routeCard/RouteCard.jsx
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { ROUTES_FETCH_SUCCESS } from '../../../constants';
 import SingleRouteInfo from './SingleRouteInfo';
 
 class RouteCard extends Component {
     componentDidMount() {
-        // dispatch action to fetch routes
-        const { routesFetch, routeName } = this.props;
-        routesFetch(routeName);
+        const { routes, status } = this.props.route;
+
+        /**
+         * FIXME: this way the routes stored to redux will only be updated with a hard refresth.
+         * That's good for less rendering, but causes outdated route info to be shown if no
+         * hard refresh is made.
+         *
+         * TODO: check route validity when mounting and only request when necessary
+         */
+
+        // do a check to avoid fetching already known routes
+        if (!routes || routes.length === 0 || status !== ROUTES_FETCH_SUCCESS) {
+            // dispatch action to fetch routes
+            const { routesFetch, routeName } = this.props;
+            routesFetch(routeName);
+        }
     }
 
     render() {
